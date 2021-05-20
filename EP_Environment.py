1ca18474d6697523ad712993a4ff4b9d513e8b4c
ingeanus <jta0031@auburn.edu>
2021-05-20 14:19:03 -0500

    Started implementing ray for multiagent

--- EP_Environment.py
+++ EP_Environment.py
@@ -4,7 +4,6 @@ from random import uniform
 from random import randint
 import re
 
-
 # Embeddings
 EMPTY_EMB = 0
 WALL_EMB = 1
@@ -16,74 +15,106 @@ LBL_ONE = "one"
 LBL_TWO = "two"
 LBL_ALL = "__all__"
 
-class EP_Environment:
+class EP_Environment(gym.Env):
+    metadata = {'render.modes': ['human']}   
     
     def generate_grid(self, height, width, density):
         grid = grid = np.zeros(width * height, int)
-        grid[0] = 2
-        grid[height*width-1] = 3
+
+        for i in range(len(self.a_agents)):
+            grid[i] = 2
+            self.a_agents.pos = i
+        for i in range(len(self.b_agents)):
+            grid[size-i-1] = 3
+            self.b_agents.pos = i
 
         for i in range(height*width):
                 if uniform(0,1) <= density and grid[i] <= 1:
                     grid[i] = 1
         return grid
 
-    def __init__(self, config: dict, one_config: dict, two_config: dict):
+    def __init__(self, config: dict, a_config: dict, _config: dict):
         self.config = config
+        self.a_config = a_config
+        self.b_config = b_config
         
+        self.a_agents = []
+        self.b_agents = []
+        for i in range(config["num_team_a"]):
+            self.a_agents.append(Agent(a_config))
+        for i in range(config["num_team_b"]):
+            self.b_agents.append(Agent(b_config))
+                    
         height = config["height"]
         width = config["width"]
         self.size = height * width
         self.grid = self.generate_grid(height, width, config["density"])
         self.base_grid = self.grid
-        self.turn = 1
+        self.turn = randint(0,1)
         self.done = False
 
-        self.one = Agent(one_config)
-        self.two = Agent(two_config)
-        self.one.location = (0)
-        self.two.location = (self.size-1)
-
-
-        self.defending = (False, False)
-        self.aiming = (False, False)
-
         # Observations:
-        # Each array index, HP1, HP2
-        self.observation_space = gym.spaces.Discrete(self.size+2)
+        # Each array index, HP for each agent
+        self.observation_space = gym.spaces.Box(low=0, 
+                                                high=max(a_config.hp, b_config.hp), 
+                                                shape=(self.size + len(a_agents) + len(b_agents),), 
+                                                dtype=np.int)
         # Actions:
         # Action: Attack a spot, Full Defense
         # Quick: Move to a spot, Aim
         # [Action, Quick Action]. Size is number of spots on board +1 each for Full Defense & Aim.
         # Last is whether to perform the Full Action (0) or Quick Action (1) first
-        self.action_space = gym.spaces.Box(low=[0,0,0], high=[self.size+1, self.size+1, 1])
+        self.action_space = gym.spaces.Box(low=np.array([0,0,0]), 
+                                           high=np.array([self.size+1, self.size+1, 1]), 
+                                           dtype=np.int)
     
-    def reset(self, one_config, two_config):
-        self.turn = 1
+    def reset(self):
+        self.turn = randint(0,1)
         self.grid = self.base_grid
-        self.one = Agent(one_config)
-        self.two = Agent(two_config)
+        self.a_agents = []
+        self.b_agents = []
+        for i in range(self.config["num_team_a"]):
+            self.a_agents.append(Agent(self.a_config))
+        for i in range(self.config["num_team_b"]):
+            self.b_agents.append(Agent(self.b_config))
+
         self.done = False
 
-        return self.get_obs();
+        testo = self.get_obs()
+        return testo
 
     def get_obs(self):
-        obs = self.grid
-        obs.append(self.hp1)
-        obs.append(self.hp2)
+        obs = []
 
-        return obs
+        for index in self.grid:
+            obs.append(index)
 
-    def calculate_reward(self):
-        one_reward = -1 + self.one.hp - self.two.hp
-        two_reward = -1 + self.two.hp - self.one.hp
+        for agent in self.a_agents:
+            obs.append(agent.hp)
+        for agent in self.b_agents:
+            obs.append(agent.hp)
 
-        if self.one.hp <= 0:
-            two_reward += 100
-        if self.two.hp <= 0:
-            one_reward += 100
+        return np.asarray(obs)
 
-        return (one_reward, two_reward)
+    def calculate_rewards(self):
+        one_reward = -1
+        two_reward = -1
+
+        for agent in a_agents:
+            one_reward += agent.hp
+            two_rewared -= agent.hp
+            
+            if agent.hp <= 0:
+                two_reward += 50 / self.config["num_a_agents"]
+
+        for agent in b_agents:
+            one_reward -= agent.hp
+            two_reward += agent.hp
+
+            if agent.hp <= 0:
+                one_reward += 50 / self.config["num_b_agents"]        
+
+        return one_reward, two_reward
 
     def take_action(self, actor, action):
         full_action = action[0]
@@ -97,76 +128,107 @@ class EP_Environment:
             take_quick_action(actor, quick_action)
             take_full_action(actor, full_action)
 
+        obs = self.get_obs()
+        reward = self.calculate_reward()[actor[0]]
+        done = self.get_dones(actor[0])
+        info = {}
+
+        return obs, reward, done, info
+       
     def take_full_action(self, actor, action):
-        if action > self.size:
-            self.defending[actor] = True
+        if action >= self.size:
+            if actor[0] == 0:
+                self.a_agents[actor[1]] = True
+            else:
+                self.b_agents[actor[1]] = True
         else:
-            if self.grid[action] > 1:
-                roll = uniform(1,100)
-                skill = self.one.skill if actor == 0 else self.two.skill
-                if aiming[actor]:
-                    skill += 10
-                    aiming[actor] = False
+            if actor[0] == 0:
+                cur_actor = self.a_agents[actor[1]]
+            else:
+                cur_actor = self.b_agents[actor[1]]
+                            
+            roll = randint(1,100)
+            skill = cur_actor.skill
+            if cur_actor.aiming:
+                skill += 10
+                cur_actor.aiming = False
 
-                if actor == 0 and self.grid[action] == 3:
-                    if roll <= self.one.skill:
-                        dmg = self.one.dmg
-                        dmg = re.split('d|\+', dmg)
+            # Determine what agent is being targeted
+            tar_actor
+            if actor[0] == 0: # If agent is on Team A, search through team B
+                for agent in self.b_agents:
+                    if agent.pos == action:
+                        tar_actor = agent
+            else:
+                for agent in self.a_agents:
+                    if agent.pos == action:
+                        tar_actor = agent
 
-                        for dice in range(dmg[0]):
-                            self.two.hp -= randint(1, dmg[1])
+            if roll <= skill:
+                roll = randint(1,100)
+                fray = round(tar_actor.fray/2, 0)
+                if tar_actor.defending:
+                    fray += 30
+                
+                if roll > fray:
+                    dmg = cur_actor.dmg
+                    dmg = re.split('d|\+', dmg)
+                    
+                    for dice in range(dmg[0]):
+                        tar_actor.hp -= randint(1, dmg[1])
 
-                        if len(dmg) > 2:
-                            self.two.hp -= dmg[2]
-                elif actor == 1 and self.grid[action] == 2:
-                    if roll <= self.two.skill:
-                        dmg = self.two.dmg
-                        dmg = re.split('d|\+', dmg)
-
-                        for dice in range(dmg[0]):
-                            self.one.hp -= randint(1, dmg[1])
-
-                        if len(dmg) > 2:
-                            self.one.hp -= dmg[2]
+                    if len(dmg) > 2:
+                        tar_actor.hp -= dmg[2]
                     
     def take_quick_action(self, actor, action):
         if action > self.size:
             self.aiming[actor] = True
         else:
-            if actor == 0 and self.grid[action] == 0:
-                self.grid[self.one.location] = 0
-                self.grid[action] = 2
-                self.one.location = action
-            elif actor == 1 and self.grid[action] == 0:
-                self.grid[self.two.location] = 0
-                self.grid[action] = 3
-                self.two.location = action
+            if actor[0] == 0:
+                cur_actor = self.a_agents[actor[1]]
+            else:
+                cur_actor = self.b_agents[actor[1]]
+
+            if self.grid[action] == 0:
+                self.grid[cur_actor.pos] = 0
+                self.grid[action] = 2 + actor[0]
+                cur_actor.pos = action
+                
+    def get_dones(self, team):
+        if team == 0:
+            for agent in self.b_agents:
+                if agent.hp > 0:
+                    return False        
+        else:
+            for agent in self.a_agents:
+                if agent.hp > 0:
+                    return False
+        return True
 
     def step(self, actions: dict):
-        if self.turn % 2 == 0:
-            self.take_action(1, actions[LBL_TWO])
-        else:
-            self.take_action(0, actions[LBL_ONE])
+        obs, rewards, dones, infp = {}, {}, {}, {}
 
-        env_rewards = self.calculate_reward()
+        if self.turn % 2 == 0:
+            for i, action in actions.items():
+                obs[i], rewards[i], dones[i], info[i] = self.take_action((1, i), action)
+        else:
+            for i, action in actions.items():
+                obs[i], rewards[i], dones[i], info[i] = self.take_action((0, i), action)
+                        
+        self.turn += 1
 
         env_obs = self.get_obs()
 
-        self.turn += 1
-
-        if self.one.hp <= 0 or self.two.hp <= 0 or self.turn >= self.config["max_turns"]:
-            done = True
+        done = self.get_dones(0) and self.get_dones(1)
 
         obs = {LBL_ONE: env_obs, LBL_TWO: env_obs}
-        rewards = {LBL_ONE: env_rewards[0], LBL_TWO: env_rewards[1]}
+        rewards = {LBL_ONE: one_reward, LBL_TWO: two_reward}
         dones = {LBL_ONE: done, LBL_TWO: done, LBL_ALL: done}
 
         return obs, rewards, dones, {}
 
     def render(self, mode='human', close=False):
-        vals = (self.grid, self.one.hp, self.two.hp)
-        print(vals)
-        return vals
+        pass
 
 class Agent:
 
@@ -175,9 +237,6 @@ class Agent:
         self.skill = config["skill"]
         self.fray = config["fray"]
         self.dmg = config["dmg"]
-
-config = {"width": 30, "height": 15, "density": 0.0, "max_turns": 25}
-one_config = {"hp": 30, "skill": 50, "fray": 40, "dmg": "2d10+6"}
-two_config = {"hp": 30, "skill": 50, "fray": 40, "dmg": "2d10+6"}
-env = EP_Environment(config, one_config, two_config)
-print(env.grid.reshape(15, 30))
+        self.pos
+        self.defending = False
+        self.aiming = False
--- EclipsePhase_ML.py
+++ EclipsePhase_ML.py
@@ -1,15 +1,32 @@
 import gym
-from EP_Environment import EP_Environment
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines.common.env_checker import check_env
+from stable_baselines import PPO2
 
-env = EP_Environment()
-for i_episode in range(20):
-    observation = env.reset()
-    for t in range(100):
-        env.render()
-        print(observation)
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        if done:
-            print("Episode finished after {} timesteps".format(t+1))
-            break
-env.close()
+import EP_Environment as epenv
+import os
+
+import ray
+from ray.tune.registry import register_env
+
+config = {"num_team_a": 1, "num_team_b": 1, "width": 5, "height": 3, "density": 0.0, "max_turns": 50}
+a_config = {"hp": 30, "skill": 50, "fray": 40, "dmg": "2d10+6"}
+b_config = {"hp": 30, "skill": 50, "fray": 40, "dmg": "2d10+6"}
+
+ray.init()
+
+env = epenv.EP_Environment(config, a_config, b_config)
+check_env(env)
+
+register_env("ep_environment", lambda _: epenv.EP_Environment(config, a_config, b_config))
+
+trainer = PPO2(env=env, config={
+    "multiagent": {
+        "policies": {
+            "one": (None, env.observation_space, env.action_space, {}),
+            "two": (None, env.observation_space, env.action_space, {}),            
+         }
+    }    
+})
+
